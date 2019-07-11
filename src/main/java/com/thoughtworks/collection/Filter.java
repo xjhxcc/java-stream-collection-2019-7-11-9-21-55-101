80d835da386ee70e50ad0114f93997ea1d24a395
xjhxcc <2351889342@qq.com>
2019-07-11 21:51:56 +0800

    completed in Filter.java unannotated functions

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -4,6 +4,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +15,23 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return array.stream().filter(item->item%2==0).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return array.stream().filter(item->item%3==0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return firstList.stream().filter(item->{
+            if (secondList.contains(item)) {
+                return true;
+            }
+            return  false;
+        }).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        return array.stream().distinct().collect(Collectors.toList());
     }
 }
